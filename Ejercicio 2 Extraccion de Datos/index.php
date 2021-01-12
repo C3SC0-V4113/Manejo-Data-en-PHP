11c2035d59a27b92a20c2224c72f37fe34f4eb8d
Francisco Valle <vallecesco@gmail.com>
2021-01-11 22:55:03 -0600

    Substring y comentarios

--- Ejercicio 2 Extraccion de Datos/index.php
+++ Ejercicio 2 Extraccion de Datos/index.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Probando la funcion de
+ * dividir un string en
+ * caracteres
+ */
+
 $data='Estudio PHP';
 
-echo $data[0];
+echo $data[0] . '<br>';
+
+/**
+ * Probando la opcion de
+ * subdividir un
+ * string
+ */
+
+$post="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, consectetur? Error est numquam, quis cumque laborum dignissimos voluptatem ex. Praesentium sunt quae facere minima at saepe magnam aperiam debitis perferendis?";
+
+$extraccion=substr($post,0,20);
+
+echo "$extraccion... [ver más]<br>";

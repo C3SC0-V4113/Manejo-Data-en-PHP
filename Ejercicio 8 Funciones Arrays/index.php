1a56e998090cfe6877e0d717e75600e96d9d3d73
Francisco Valle <vallecesco@gmail.com>
2021-01-13 17:44:53 -0600

    Practicando comandos más complejos

--- Ejercicio 8 Funciones Arrays/index.php
+++ Ejercicio 8 Funciones Arrays/index.php
@@ -1,5 +1,7 @@
 <?php
 
+//Sort normal
+
 $courses=['php','javascript','laravel'];
 sort($courses); //rsort() para reversible
 
@@ -9,16 +11,39 @@ foreach ($courses as $key => $curso) {
 }
 echo '</ul>';
 
+//Sort por llaves
+
 $cursos=[
     10=>'php',
     100=>'javascript',
     1000=>'laravel'
 ];
 
-ksort($cursos);
+ksort($cursos); //krsort(es reversible)
 
 echo '<ul>';
 foreach ($cursos as $key => $curso) {
     echo "<li>$curso</li>";
 }
 echo '</ul>';
+
+/**
+ * Usando slice para quitar
+ * n cantidad de porciones
+ */
+
+echo '<ul>';
+foreach (array_slice($cursos,1) as $key => $curso) {
+    echo "<li>$curso</li>";
+}
+echo '</ul>';
+
+/**
+ * Usando chunk, este divide el arreglo
+ * en n cantidad de arreglos
+ */
+
+var_dump(array_chunk($courses,3));
+
+//Elimina el primer elemento y lo retorna
+array_shift($courses);

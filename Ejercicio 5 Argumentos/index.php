7e9bbc84e81da9f932275d8218e9df5b6c82eea0
Francisco Valle <vallecesco@gmail.com>
2021-01-13 12:48:05 -0600

    Entendiendo argumentos

---
+++ Ejercicio 5 Argumentos/index.php
@@ -0,0 +1,34 @@
+<?php
+
+//Valores
+function saludo($nombre){
+    return "Hola, $nombre";
+}
+
+echo '<p>'.saludo('Cesco').'</p>';
+
+
+//Referencias
+
+/**
+ * De no ser por el ampersand(&)
+ * el segundo echo fuera de la
+ * funcion, diria la variable
+ * original (PHP)
+ */
+$cursos='PHP';
+function path(&$cursos){
+    $cursos='Laravel';
+    echo '<p>'.$cursos.'</p>'; //Laravel
+}
+
+path($cursos);
+echo '<p>'.$cursos.'</p>'; //Laravel
+
+//Predeterminado
+function holi($name='Cesco'){
+    return "Holi, $name";
+}
+
+echo '<p>'.holi().'</p>';
+echo '<p>'.holi("Karla").'</p>';

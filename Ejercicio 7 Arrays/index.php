a954c68ed6ea91c8fde08c87f387e8aaaca780c4
Francisco Valle <vallecesco@gmail.com>
2021-01-13 15:30:53 -0600

    entendiendo los arrays dobles

--- Ejercicio 7 Arrays/index.php
+++ Ejercicio 7 Arrays/index.php
@@ -1,5 +1,13 @@
 <?php
 
+echo "<h1>Arrays</h1>";
+
+echo "<h2>Arrays Simples</h2>";
+
+/**
+ * Arrays Simples
+ */
+
 $cursos=['javascript','laravel','php','vuejs'];
 
 echo '<ul>';
@@ -8,8 +16,40 @@ foreach ($cursos as $key => $curso) {
 }
 echo '</ul>';
 
+/*
 echo '<ul>';
 for ($i=0; $i < count($cursos); $i++) { 
     echo "<li>[$i]$cursos[$i]</li>";
 }
+echo '</ul>';
+*/
+
+/**
+ * Arrays Dobles
+ */
+echo "<h2>Arrays Dobles</h2>";
+
+$courses=[
+    'frontend'=>'javascript',
+    'framework'=>'laravel',
+    'backend'=>'php'
+];
+
+echo '<ul>';
+foreach ($courses as $key => $curso) {
+    echo "<li>[$key]$curso</li>";
+}
+echo '</ul>';
+
+/**
+ * Usando array_walk con el
+ * objetivo de iterar
+ */
+
+function upper($curso, $llave){
+    echo '<li>'.strtoupper($curso).": $llave".'</li>';
+}
+
+echo '<ul>';
+array_walk($courses,'upper');
 echo '</ul>';

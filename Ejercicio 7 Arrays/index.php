87ce7d9ab6f64963e192ab7b90238adfa620c0ca
Francisco Valle <vallecesco@gmail.com>
2021-01-13 14:26:16 -0600

    Haciendo experimentos con los bloques de codigo

--- Ejercicio 7 Arrays/index.php
+++ Ejercicio 7 Arrays/index.php
@@ -1,3 +1,15 @@
 <?php
 
-$cursos=[];
+$cursos=['javascript','laravel','php','vuejs'];
+
+echo '<ul>';
+foreach ($cursos as $key => $curso) {
+    echo "<li>[$key]$curso</li>";
+}
+echo '</ul>';
+
+echo '<ul>';
+for ($i=0; $i < count($cursos); $i++) { 
+    echo "<li>[$i]$cursos[$i]</li>";
+}
+echo '</ul>';
